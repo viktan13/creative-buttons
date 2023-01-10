5717d8f8ee68747d9b50e3c9a4ae3872f2559022
Viktor <vik.tan@hotmail.com>
2023-01-09 18:38:55 -0600

    created second button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Button1 from "./Button1";
+import Button2 from "./Button2";
 
 function App() {
   return (
     <div className="App">
-      <Button1/>
+      <Button2/>
     </div>
   );
 }
--- src/Button2.js
+++ src/Button2.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './App.css'
 const Button2 = () => {
     return (
         <div className="btn2">
-
+            <a href="">Hover Me</a>
         </div>
     );
 };
